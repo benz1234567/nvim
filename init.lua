04b0b4d4377303b6f6d56fc9c777ae9ad3279100
Ben Z <benzgherea@gmail.com>
2024-07-18 09:20:17 -0700

    first commit

---
+++ init.lua
@@ -0,0 +1,68 @@
+-- Plugins from pm plugin manager
+vim.cmd.source("/home/benny/.config/nvim/pm.vim")
+
+-- Allow you to access any file in any subdirectory with :find
+vim.o.path = '.,**,'
+
+
+-- Status bar settings, shows the current file all the way to the right
+-- vim.api.nvim_set_hl(0, 'blue', { fg = '#ffffff', bg = '#16243b' })
+-- vim.api.nvim_set_hl(0, 'black', { fg = '#ffffff', bg = '#000000' })
+-- vim.api.nvim_set_hl(0, 'default', { fg = '#e0e2ea', bg = '#14161b' })
+-- vim.o.statusline = "%#default#%=%f  "
+
+-- Removes the status bar:
+vim.opt.laststatus = 0
+
+-- Set colorscheme
+--vim.cmd('colorscheme wildcharm')
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Shows trailing space as ·
+vim.opt.list = true
+vim.opt.listchars = { trail = '·', tab = '  ' }
+
+-- Makes tabs two spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
+
+-- Shows substitution commands as what they'll look like live
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+
+-- Keeps cursor in center of screen
+vim.opt.scrolloff = 100
+
+-- Escape removes search higlighting
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Flash highlight yanks
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Keymaps:
+--
+-- Leader+y yanks to system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>f', '<cmd>FZF<CR>')
---
+++ pm.vim
@@ -0,0 +1,5 @@
+" Plugins from pm plugin manager
+let g:plugins = [
+  \ 'https://github.com/ThePrimeagen/vim-be-good',
+  \ 'https://github.com/benz1234567/vim-be-better',
+  \ 'https://github.com/junegunn/fzf']
